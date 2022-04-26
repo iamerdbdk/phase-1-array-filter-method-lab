b3dcf7158096ec829e01a0a575704822987b9ed2
Erdal Budak <erdalbudak89@gmail.com>
2022-04-26 11:16:24 -0400

    commit tree 99ba63766b279897bf307fddc1664bb0a1987676

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-// Code your solution here
+function findMatching(source, erdal) {
+    return source.filter(
+    (possibleMatch) => possibleMatch.toLowerCase() === erdal.toLowerCase()
+    );
+}
+
+function fuzzyMatch(source, testString) {
+    return source.filter(
+    (possibleMatch) =>
+        possibleMatch.toLowerCase().indexOf(testString.toLowerCase()) === 0
+    );
+}
+
+function matchName(source, erdalName) {
+    return source.filter((record) => record.name === erdalName);
+}
